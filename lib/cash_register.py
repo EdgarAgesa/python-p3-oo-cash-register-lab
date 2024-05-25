354f1de2fda9613f0869d6c737bb9b3b9e13dba4
Edgar <edgarasige@gmail.com>
2024-05-25 13:18:18 +0300

    Completed assingments

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,37 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        for _ in range(quantity):
+            self.items.append(item)
+
+    def apply_discount(self):
+        
+        if self.discount == 0:
+            print("There is no discount to apply.")
+            return "There is no discount to apply."
+
+        discount_amount = self.total * self.discount / 100
+        self.total -= discount_amount
+        self.total = round(self.total)  
+        print(f"After the discount, the total comes to ${self.total}.")
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item = self.items.pop()
+            if isinstance(last_item, tuple):
+                last_price = last_item[1] * last_item[2]  # Calculate total price for multiple items
+            else:
+                last_price = last_item
+
+            try:
+                last_price = float(last_price)  # Convert the total price to float
+            except ValueError:
+                print("Error: Unable to convert price to float.")
+                return
+
+            self.total -= last_price
